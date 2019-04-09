ec3206ecb5721720298283d973d93304ec10c3e3
Maxim Kochukov <kochukov.ma@phystech.edu>
2019-04-10 00:39:56 +0300

    enh: release bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='randcrack',  # How you named your package folder (MyLib)
     packages=['randcrack'],  # Chose the same as "name"
-    version='0.1',  # Start with a small number and increase it with every change you make
+    version='0.1.1',  # Start with a small number and increase it with every change you make
     license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='Predict python\'s random module random generated values',
     author='Maxim Kochukov',  # Type in your name
