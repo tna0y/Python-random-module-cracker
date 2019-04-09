5a48a8db4434b480c57c2dc447c7ba928817574e
Maxim Kochukov <kochukov.ma@phystech.edu>
2019-04-10 00:36:17 +0300

    enh: CPython only

--- setup.py
+++ setup.py
@@ -26,6 +26,6 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: CPython',
     ],
 )
