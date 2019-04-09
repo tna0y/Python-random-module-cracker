7e762049b2b8a753b07c5bd006a8e405520c83df
Maxim Kochukov <kochukov.ma@phystech.edu>
2019-04-10 01:49:57 +0300

    new: manifest

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 setup(
     name='randcrack',  # How you named your package folder (MyLib)
     packages=['randcrack'],  # Chose the same as "name"
-    version='0.1.2',  # Start with a small number and increase it with every change you make
+    version='0.1.3',  # Start with a small number and increase it with every change you make
     license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='Predict python\'s random module random generated values',
     long_description=long_description,
@@ -16,7 +16,7 @@ setup(
     author_email='kochukov.ma@gmail.com',  # Type in your E-Mail
     url='https://github.com/tna0y/Python-random-module-cracker',  # Provide either the link to your github
     # or to your website
-    download_url='https://github.com/tna0y/Python-random-module-cracker/archive/0.1.2.tar.gz',
+    download_url='https://github.com/tna0y/Python-random-module-cracker/archive/0.1.3.tar.gz',
     keywords=['random', 'security', 'cryptography', 'cracker', 'encryption'],  # Keywords that define your package best
     install_requires=[],
     classifiers=[
