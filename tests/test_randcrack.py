2f860b0ad2439adfb82701e859b85a8f8719bf7d
Jorian Woltjer <jorianwoltjer@hotmail.com>
2023-05-05 10:37:09 +0200

    Implement offsetting to previous internal states

--- tests/test_randcrack.py
+++ tests/test_randcrack.py
@@ -11,7 +11,7 @@ def test_submit_not_enough():
 
     cracker = RandCrack()
 
-    for i in range(623):
+    for _ in range(623):
         cracker.submit(random.randint(0, 4294967294))
 
     with pytest.raises(ValueError):
@@ -23,11 +23,11 @@ def test_submit_too_much():
 
     cracker = RandCrack()
 
-    for i in range(624):
+    for _ in range(624):
         cracker.submit(random.randint(0, 4294967294))
 
     with pytest.raises(ValueError):
-        cracker.submit(random.randint(0, 4294967294))
+        cracker.submit(random.getrandbits(32))
 
 
 def test_predict_first_624():
@@ -35,7 +35,7 @@ def test_predict_first_624():
 
     cracker = RandCrack()
 
-    for i in range(624):
+    for _ in range(624):
         cracker.submit(random.randint(0, 4294967294))
 
     assert sum([random.getrandbits(32) == cracker.predict_getrandbits(32) for _ in range(1000)]) == 1000
@@ -46,16 +46,58 @@ def test_predict_first_1000_close():
 
     cracker = RandCrack()
 
-    for i in range(624):
+    for _ in range(624):
         cracker.submit(random.randint(0, 4294967294))
 
     assert sum([random.getrandbits(32) == cracker.predict_getrandbits(32) for _ in range(1000)]) == 1000
+
 
 def test_predict_random():
     random.seed(time.time())
 
     cracker = RandCrack()
 
-    for i in range(624):
+    for _ in range(624):
         cracker.submit(random.randint(0, 4294967294))
+
     assert sum([random.random() == cracker.predict_random() for _ in range(1000)]) == 1000
+
+
+def test_predict_previous():
+    random.seed(time.time())
+
+    unknown = [random.getrandbits(32) for _ in range(1000)]
+
+    cracker = RandCrack()
+
+    for _ in range(624):
+        cracker.submit(random.getrandbits(32))
+
+    cracker.offset(-624)
+    cracker.offset(-1000)
+
+    assert unknown == [cracker.predict_getrandbits(32) for _ in range(1000)]
+
+
+def test_predict_previous_randbelow():
+    random.seed(time.time())
+
+    # randint() uses randbelow() internally
+    unknown = [random.randint(1, 800) for _ in range(1000)]
+
+    cracker = RandCrack()
+
+    for _ in range(624):
+        cracker.submit(random.getrandbits(32))
+
+    cracker.offset(-624)
+    cracker.offset(-2000)  # Go back too far to include everything
+
+    guesses = [cracker.predict_randint(1, 800) for _ in range(2000)]
+
+    def is_subseq(x, y):
+        return all(any(c == ch for c in y) for ch in x)
+
+    # The sequence of unknown numbers should be in guesses, but there may be numbers before and after
+    # (e.g. if the sequence is [1, 2, 3], guesses may be [123, 42, 1, 2, 3, 1337])
+    assert is_subseq(unknown, guesses)
